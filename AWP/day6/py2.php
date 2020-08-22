fa0f233e5e10058ddfad7c6b2d47a19049f6ce9f
NiK <nitinkumardj@live.com>
2020-08-22 11:39:03 +0530

    server calls

---
+++ AWP/day6/py2.php
@@ -0,0 +1,21 @@
+<?php
+//$a=$_POST["xyz"];
+$c='{"num":3}';
+$b=json_decode($c);
+$toclient["status"]=0;
+$arr= array("1","2","3","4");
+$arrlength = count($arr);
+
+for($i=0;$i<$arrlength;$i++){
+	// echo $arr[$i];
+	if($arr[$i]==$b->num){
+		echo $arr[$i];
+		$toclient["status"]=1;
+		break;
+	}
+	else
+		$toclient["status"]=0;
+}
+$abc=json_encode($toclient);
+echo $abc;
+?>
